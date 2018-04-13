c60524cc7867c7e74ebab94ca8c9334a6c70409a
Elijah Bansley <elijahbansley@monroe.k12.wi.us>
2018-04-12 22:26:44 -0500

    Yay beholder

--- bot.py
+++ bot.py
@@ -25,6 +25,18 @@ async def beholder(ctx):
     embed.set_image(url="http://media.wizards.com/2014/images/dnd/newtodnd/Beholder_1E.jpg")
     await client.send_message(ctx.message.channel, embed=embed)
     if(ctx.message.author.server_permissions.administrator):
+        embed.add_field(name="Size/Type", value="Large Aberration", inline=True)
+        embed.add_field(name="Hit Dice", value="11d8+44 (93 hp)", inline=True)
+        embed.add_field(name="Initiative", value="+6", inline=False)
+        embed.add_field(name="Speed", value="5ft. (1 square), fly 20ft. (good)", inline=False)
+        embed.add_field(name="Armor Class", value="26 (-1 size, +2 Dex, +15 natural), touch 11, flat-footed 24", inline=False)
+        embed.add_field(name="Base Attack/Grapple", value="+8/+12", inline=False)
+        embed.add_field(name="Attack", value="Eye rays +9 ranged touch and bite +2 melee (2d4)", inline=False)
+        embed.add_field(name="Full Attack", value="Same as attack", inline=False)
+        embed.add_field(name="Space/Reach", value="10ft./5ft.", inline=False)
+        embed.add_field(name="Special Attacks", value="Eye rays", inline=False)
+        embed.add_field(name="Special Qualities", value="All-around vision, antimagic cone, darkvision 60 ft., and flight.", inline=False)
+        embed.add_field(name="Enviroment", value="Cold hills", inline=False)
         await client.send_message(ctx.message.author, embed=embed)
 
 client.run('NDM0MTY1MDU1MzE2Mjk1Njkw.DbGbUw.Tz665JQ8lnMKD8WIx6pcXko6TgA')
