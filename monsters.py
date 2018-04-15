30796ebb0cd0ebb21d4451b36d4403b1eff63c15
elijahbansley <elijahbansley@monroe.k12.wi.us>
2018-04-15 18:54:31 -0400

    External text support

---
+++ monsters.py
@@ -0,0 +1,8 @@
+#Declaring vars goes like this:
+#
+# beholder_desc = ""
+# beholder_charisma = ""
+# etc for each monster
+#
+
+beholder_desc = '"It floats before you, a bulbous body with a central, unblinking eye, and a large maw filled with daggerlike teeth. Smaller eyes, attached to wriggling stalks, sprout from the top of the orblike body."'
