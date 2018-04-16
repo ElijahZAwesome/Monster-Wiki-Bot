83c4489e2c7b49ce62ad63dbef38e28dfbdb801d
elijahbansley <elijahbansley@monroe.k12.wi.us>
2018-04-15 20:21:35 -0400

    Beholder is now entirely external

--- monsters.py
+++ monsters.py
@@ -1,8 +1,46 @@
 #Declaring vars goes like this:
 #
-# beholder_desc = ""
-# beholder_charisma = ""
-# etc for each monster
+#in the file, all ya gotta do is specify 
 #
+#<monster>_<info> = "value"
+#
+#info can be:
+#
+#size
+#hitpoints
+#speed
+#strength
+#dexterity
+#constitution
+#intelligence
+#wisdom
+#charisma
+#senses
+#language
+#challenge
+#dice
+#initiative
+#armor
+#baseattack
+#attack
+#fullattack
+#reach
+#specialattack
+#specialquality
+#enviroment
+#
+#Remember that you dont have to specify everything, like senses for example
 
 beholder_desc = '"It floats before you, a bulbous body with a central, unblinking eye, and a large maw filled with daggerlike teeth. Smaller eyes, attached to wriggling stalks, sprout from the top of the orblike body."'
+beholder_size = "Large Aberration"
+beholder_dice = "11d8+44 (93 hp)"
+beholder_initiative = "+6"
+beholder_armor = "26 (-1 size, +2 Dex, +15 natural), touch 11, flat-footed 24"
+beholder_speed = "5ft. (1 square), fly 20ft. (good)"
+beholder_baseattack = "+8/+12"
+beholder_attack = "Eye rays +9 ranged touch and bite +2 melee (2d4)"
+beholder_fullattack = "Same as attack"
+beholder_reach = "10ft./5ft."
+beholder_specialattack = "Eye rays"
+beholder_specialquality = "All-around vision, antimagic cone, darkvision 60 ft., and flight."
+beholder_enviroment = "Cold hills"
