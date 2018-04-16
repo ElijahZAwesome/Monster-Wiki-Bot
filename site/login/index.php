7b8802b6b562c208f86884912979dea881fdc0b2
elijahbansley <elijahbansley@monroe.k12.wi.us>
2018-04-16 13:45:49 -0400

    Changed method to POST to fit PHP

--- site/login/index.php
+++ site/login/index.php
@@ -45,7 +45,7 @@ else
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100 p-t-85 p-b-20">
-				<form class="login100-form validate-form">
+				<form class="login100-form validate-form" method="POST" action="index.php">
 					<span class="login100-form-title p-b-70">
 						Welcome
 					</span>
