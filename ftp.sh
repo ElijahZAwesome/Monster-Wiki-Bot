396a7b4fd5aba464a712ffb41390fe1b023f9507
elijahbansley <elijahbansley@monroe.k12.wi.us>
2018-04-25 04:24:58 -0400

    Autogenerated commit

--- ftp.sh
+++ ftp.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-HOST='ftpupload.net'
-USER='epiz_21976851'
-PASSWD='P3P3lord'
-
-ftp -n $HOST <<END_SCRIPT
-quote USER $USER
-quote PASS $PASSWD
+ftp -n ftpupload.net 
+quote USER epiz_21976851
+quote PASS P3P3lord
 MPUT *
 quit
-END_SCRIPT
 exit 0
