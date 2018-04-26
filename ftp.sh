7f964682c84472f9649b36204272ae3abd1c7ae9
elijahbansley <elijahbansley@monroe.k12.wi.us>
2018-04-26 12:46:12 -0400

    stuffs

--- ftp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "set ssl:verify-certificate no" >> ~/workspace/ftp.sh
-set ssl:verify-certificate no
-lftp -c "set ftp:list-options -a;
-open ftp://epiz_21976851:P3P3lord@ftpupload.net;
-lcd ./site/DDLC;
-cd /htdocs;
-mirror --reverse --delete --use-cache --verbose --allow-chown --allow-suid --no-umask --parallel=2 --exclude-glob .svn"set ssl:verify-certificate no
